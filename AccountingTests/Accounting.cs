fd903cf81704727771df6574e2bb72238cff6f67
sheraium <sheraium@gmail.com>
2019-02-16 23:52:40 +0800

    pass

--- AccountingTests/Accounting.cs
+++ AccountingTests/Accounting.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AccountingTests
@@ -15,8 +16,27 @@ namespace AccountingTests
         public double TotalAmount(DateTime start, DateTime end)
         {
             var listOfBudgets = _budgetRepo.GetAll();
-            var yearMonth = start.ToString("yyyyMM");
-            return listOfBudgets.FirstOrDefault(b => b.YearMonth == yearMonth)?.Amount ?? 0;
+            var listOfDays = new Dictionary<DateTime, int>();
+            while (start <= end)
+            {
+                var yearMonth = new DateTime(start.Year, start.Month, 1);
+                if (listOfDays.ContainsKey(yearMonth))
+                {
+                    listOfDays[yearMonth]++;
+                }
+                else
+                {
+                    listOfDays.Add(yearMonth, 1);
+                }
+                start = start.AddDays(1);
+            }
+
+            return listOfDays.Sum(d =>
+             {
+                 var amount = listOfBudgets.FirstOrDefault(b => b.YearMonth == d.Key.ToString("yyyyMM"))?.Amount ?? 0;
+                 var rate = (double)d.Value / DateTime.DaysInMonth(d.Key.Year, d.Key.Month);
+                 return amount * rate;
+             });
         }
     }
 }
