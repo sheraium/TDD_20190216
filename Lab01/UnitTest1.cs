879976518447d82184c1ada1da08405a844a42b0
320 陳永修 <peterch@mirle.com.tw>
2019-02-16 22:12:20 +0800

    Add project files.

---
+++ Lab01/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab01
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
